40525b9e20f8b12ca884cb91813a311a85fff915
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-10-03 16:35:29 +0900

    2021.10.03 약수 구하기

--- 01회차 숙제/약수 구하기 /FindAFactor/FindAFactor/main.swift
+++ 01회차 숙제/약수 구하기 /FindAFactor/FindAFactor/main.swift
@@ -7,8 +7,19 @@
 
 import Foundation
 
-let number = readLine()!
+let num = readLine()!.split(separator: " ").map{ Int($0) }
 
-for _ in number {
-    
+var array : [Int] = []
+let n = num[0]!
+let k = num[1]!
+
+for i in 1 ... n {
+    if n % i == 0 {
+        array.append(i)
+    }
+}
+if array.count < k {
+    print(0)
+}else {
+    print(array[k-1])
 }
